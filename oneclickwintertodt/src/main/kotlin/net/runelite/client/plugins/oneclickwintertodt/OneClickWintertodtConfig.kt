131dd0cac1ca45c6dbddecf328e932769360f192
Rebecca Davies <email@rebecca.sh>
2022-05-19 17:20:10 +1000

    Fix config settings, new jar released for wintertodt.

--- oneclickwintertodt/src/main/kotlin/net/runelite/client/plugins/oneclickwintertodt/OneClickWintertodtConfig.kt
+++ oneclickwintertodt/src/main/kotlin/net/runelite/client/plugins/oneclickwintertodt/OneClickWintertodtConfig.kt
@@ -6,39 +6,8 @@ import net.runelite.client.config.*
 
 @ConfigGroup("OneClickWintertodt")
 interface OneClickWintertodtConfig : Config {
-    companion object {
-        @JvmField
-        @ConfigTitle(
-            keyName = "foodConfig",
-            name = "OneClickWintertodt Settings",
-            description = "Configure your plugins settings to suit your needs",
-            position = 1)
-        val food: String = "food"
-    }
 
-    @ConfigItem(
-        keyName = "food",
-        name = "Select food",
-        description = "Select the food to eat",
-        position = 2,
-        title = "foodConfig")
-    @JvmDefault
-    fun food(): Food {
-        return Food.CAKE
-    }
-
-    @ConfigItem(
-        position = 3,
-        keyName = "food",
-        name = "Eat health",
-        description = "Set the health your player will eat at",
-        title = "foodConfig")
-    @JvmDefault
-    fun health(): Int {
-        return 25
-    }
-
-    enum class Food(private val food: String, val id: List<Int>) {
+    enum class Food(private val type: String, val id: List<Int>) {
         WINE("Wine", listOf(ItemID.JUG_OF_WINE)),
         CAKE("Cake", listOf(ItemID.SLICE_OF_CAKE, ItemID._23_CAKE, ItemID.CAKE)),
         TROUT("Trout", listOf(ItemID.TROUT)),
@@ -47,7 +16,42 @@ interface OneClickWintertodtConfig : Config {
         SHARK("Shark", listOf(ItemID.SHARK));
 
         override fun toString(): String {
-            return food
+            return type
         }
     }
+
+    companion object {
+        @JvmField
+        @ConfigTitle(
+            keyName = "foodConfig",
+            name = "OneClickWintertodt Settings",
+            description = "Configure your plugins settings to suit your needs",
+            position = 1
+        )
+        val foodConfig: String = "foodConfig"
+    }
+
+    @ConfigItem(
+        keyName = "food",
+        name = "Select food",
+        description = "Select the food to eat",
+        position = 2,
+    )
+    @JvmDefault
+    fun food(): Food {
+        return Food.CAKE
+    }
+
+    @ConfigItem(
+        position = 3,
+        keyName = "health",
+        name = "Eat health",
+        description = "Set the health your player will eat at",
+    )
+    @JvmDefault
+    fun health(): Int {
+        return 25
+    }
 }
+
+
